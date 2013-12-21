6dc0ae866f528b217032f0c103beb5c6fd611e2c
James Childress <james.childress@me.com>
2013-12-21 08:07:29 -0800

    'before' and 'after' parameters for filtering dispatches by date

--- pdx911/app/routes.rb
+++ pdx911/app/routes.rb
@@ -33,15 +33,18 @@ end
 # 
 # Params
 # - callback : the name of the JSONP callback function
-# - offset   : the number of results by which the query is offset
-# - agency   : the ID of the agency by which dispatches are filtered
-# - category : the ID of the category by which dispatches are filtered
-# - date     : TODO
+# - offset   : the number of dispatches by which the results are offset
+# - agency   : only return dispatches with this agency ID
+# - category : only return dispatches with this category ID
+# - before   : only return dispatches that occurred before this time (in seconds since the Unix Epoch) 
+# - after    : only return dispatches that occurred on or after this time (in seconds since the Unix Epoch) 
 get '/dispatches' do
   
   where = [];
-  where << "agency_id = #{params[:agency].to_i}" if params[:agency]
-  where << "category_id = #{params[:category].to_i}" if params[:category]
+  where << "agency_id = #{params[:agency].to_i}"       if params[:agency]
+  where << "category_id = #{params[:category].to_i}"   if params[:category]
+  where << "date < '#{Time.at(params[:before].to_i)}'" if params[:before]
+  where << "date >= '#{Time.at(params[:after].to_i)}'" if params[:after]
   where = where.join " AND "
   where = "WHERE " + where unless where.empty?
   
