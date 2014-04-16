26bf40b434f7299001647649b9fc23ec6f1b51fa
James Childress <james.childress@me.com>
2014-04-15 18:09:14 -0700

    added sslmode to db connection options

--- pdx911/app/config.rb
+++ pdx911/app/config.rb
@@ -1,10 +1,11 @@
 configure :production, :development do
   
   set :database_connection, {
-    host:     'localhost',
-    dbname:   ENV['PDX911_DATABASE_NAME'],
-    user:     ENV['PDX911_READONLY_DATABASE_USER'],
-    password: ENV['PDX911_READONLY_DATABASE_PASSWORD']
+    host:     'localhost'                              ,
+    dbname:   ENV['PDX911_DATABASE_NAME']              ,
+    user:     ENV['PDX911_READONLY_DATABASE_USER']     ,
+    password: ENV['PDX911_READONLY_DATABASE_PASSWORD'] ,
+    sslmode:  ENV['PDX911_READONLY_DATABASE_SSL_MODE']
   }
 
   set :max_results, 100
@@ -16,8 +17,8 @@ end
 configure :test do
 
   set :database_connection, {
-    host:   'localhost',
-    dbname: 'pdx911_test',
+    host:   'localhost'   ,
+    dbname: 'pdx911_test' ,
     options: '--client-min-messages=warning'
   }
 
